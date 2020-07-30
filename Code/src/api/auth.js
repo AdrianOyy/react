9406d5b833d61f1602e6e15bc33125d5495aa29c
amaneru <amaneru@163.com>
2020-07-30 15:41:26 +0800

    asd

--- Code/src/api/auth.js
+++ Code/src/api/auth.js
@@ -2,7 +2,7 @@ import request from '../utils/request'
 
 class Auth {
   login(params, options) {
-    return request.get('/AAA/syncUser/login', params, options)
+    return request.get('/syncUser/login', params, options)
     // return request.post('/syncUser/login', params, options)
   }
 }
