13b9e9d04b581b334dc2288ed9eb2988269a0319
Tom <tomqi@apjcorp.com>
2020-08-31 16:56:43 +0800

    myapproval列表界面增加显示Create By

--- Code/src/pages/workFlow/MyApproval/components/Detail/Detail.js
+++ Code/src/pages/workFlow/MyApproval/components/Detail/Detail.js
@@ -89,6 +89,7 @@ function Create() {
           // DialogField(dyform.dynamicSon, dyform.sonList)
         })
       })
+  // eslint-disable-next-line
   }, [ id, processDefinitionId, userId ])
 
   useEffect(() => {
@@ -209,24 +210,16 @@ function Create() {
   }
 
   const handleT1FollowUpClick = () => {
-    const data = {
-      taskId: id,
-      variables: { followUp: 'T1' },
-    }
+    // const data = {
+    //   taskId: id,
+    //   variables: { followUp: 'T1' },
+    // }
     alert('T1 followUp')
   }
   const handleT2FollowUpClick = () => {
-    const data = {
-      taskId: id,
-      variables: { followUp: 'T2' },
-    }
     alert('T2 followUp')
   }
   const handleT6FollowUpClick = () => {
-    const data = {
-      taskId: id,
-      variables: { followUp: 'T6' },
-    }
     alert('T6 followUp')
   }
 
