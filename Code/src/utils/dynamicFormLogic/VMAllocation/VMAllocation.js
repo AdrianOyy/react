e3e2a5b9c70173f91f5ac1efe1e57d93a7484b66
amaneru <amaneru@163.com>
2020-09-01 15:12:19 +0800

    logic update

--- Code/src/utils/dynamicFormLogic/VMAllocation/VMAllocation.js
+++ Code/src/utils/dynamicFormLogic/VMAllocation/VMAllocation.js
@@ -7,7 +7,7 @@ export default class VMAllocation {
       dataList[i].value = formatDateTime(value)
     } else {
       switch (id) {
-        case 'Project_Name':
+        case 'tenant':
           dataList[i].value = value
           // eslint-disable-next-line no-case-declarations
           // const itemDetail = dataList[i].itemList.find(t => t.id === value)
@@ -37,18 +37,18 @@ export default class VMAllocation {
       dataList[i].value = formatDateTime(value)
     } else {
       switch (id) {
-        case 'CPU_request_number':
+        case 'cpu_request_number':
           dataList[i].value = parseInt(value)
           for (let j = 0; j < dataList.length; j++) {
-            if (dataList[j].id === 'RAM_request_number') {
+            if (dataList[j].id === 'ram_request_number') {
               dataList[j].value = 8 * parseInt(value)
-              const ramInput = document.getElementById('RAM_request_number')
+              const ramInput = document.getElementById('ram_request_number')
               ramInput.value = 8 * parseInt(value)
               const next = ramInput.nextElementSibling || ramInput.nextSibling
               const target = next.firstChild || next.firstElementChild
               target.style = "width: auto;height: 11px;display: block;padding: 0;font-size: 0.75em;max-width: 0.01px;text-align: left;transition: max-width 50ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;visibility: hidden;max-width: 1000px;transition: max-width 100ms cubic-bezier(0.0, 0, 0.2, 1) 50ms;"
               // target.className = 'jss508 jss509 PrivateNotchedOutline-legendLabelled-508 PrivateNotchedOutline-legendNotched-509'
-              const ramLabel = document.getElementById('RAM_request_number-label')
+              const ramLabel = document.getElementById('ram_request_number-label')
               // console.log(ramLabel)
               ramLabel.className = 'MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-shrink MuiInputLabel-outlined MuiFormLabel-filled'
               break
@@ -61,19 +61,20 @@ export default class VMAllocation {
     }
   }
 
-  checkDialog(values) {
-    const { CPU_request_number, RAM_request_number } = values
-    if (isNaN(parseInt(CPU_request_number))) {
-      CommonTip.error('cpu only receive a integer')
+  async checkDialog(values) {
+    const { cpu_request_number, ram_request_number } = values
+    if (isNaN(parseInt(cpu_request_number))) {
+      CommonTip.err('cpu only receive a integer')
       return false
     }
-    if (isNaN(parseInt(CPU_request_number))) {
-      CommonTip.error('ram only receive a integer')
+    if (isNaN(parseInt(ram_request_number))) {
+      CommonTip.err('ram only receive a integer')
       return false
     }
-    if (parseInt(RAM_request_number) / parseInt(CPU_request_number) > 8) {
-      CommonTip.error('ram is 8 times larger than cpu')
+    if (parseInt(ram_request_number) / parseInt(cpu_request_number) > 8) {
+      CommonTip.err('ram is 8 times larger than cpu')
       return false
     }
+    return true
   }
 }
