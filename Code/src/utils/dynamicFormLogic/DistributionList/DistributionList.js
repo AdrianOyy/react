98b3b81e1f82a82ee63047d9d97a198556c4dbfd
Tom <tomqi@apjcorp.com>
2020-10-15 16:08:26 +0800

    add logic, update email 验证

---
+++ Code/src/utils/dynamicFormLogic/DistributionList/DistributionList.js
@@ -0,0 +1,137 @@
+// import Api from "../../../api/accountManagement"
+import CommonTip from "../../../components/CommonTip"
+
+export default class DistributionList {
+  // eslint-disable-next-line
+  async onFieldChange(data, dataMap, ref) {
+    const { id } = data
+    dataMap.set(id, data)
+  }
+
+  // eslint-disable-next-line
+  async checkChildForm(childDataMap) {
+    return true
+  }
+
+
+  async checkForm(parentFormDetail, parentDataMap) {
+    let pass = true
+    // 验证必填字段
+    for (let i = 0; i < parentFormDetail.length; i++) {
+      const { required, fieldName, fieldDisplayName } = parentFormDetail[i]
+      if (required && (!parentDataMap.get(fieldName) || !parentDataMap.get(fieldName).value)) {
+        CommonTip.error(`${fieldDisplayName} is required`)
+        pass = false
+        break
+      }
+    }
+    return pass
+  }
+
+  // 处理父表数据表
+  handleParentDefaultData(rawData, stepName) {
+    console.log('111111111', rawData)
+    return rawData
+  }
+
+  handleParentStartData() {
+    const data = {}
+    return data
+  }
+
+  // 处理子表数据表
+  handleChildDefaultData(rawData, childDataListMap) {
+    const childList = []
+    for (let i = 0; i < rawData.length; i++) {
+      const el = rawData[i]
+      const childModel = {}
+      for (let key in el) {
+        const child = childDataListMap.get(key)
+        if (key === 'id') {
+          const model = {
+            id: 'id',
+            value: el[key],
+            label: el[key],
+          }
+          Object.assign(childModel, { id: model })
+          Object.assign(childModel, { checkState: false })
+        } else {
+          if (!child) continue
+          const model = {
+            id: child.fieldName,
+            value: child.type === 'select' ? child.itemList.find(t => t[child.valueField].toString() === el[key].toString())[child.valueField] : el[key],
+            label: child.type === 'select' ? child.itemList.find(t => t[child.valueField].toString() === el[key].toString())[child.labelField] : el[key],
+          }
+          Object.assign(childModel, { [child.fieldName]: model })
+        }
+      }
+      childList.push(childModel)
+    }
+    return childList
+  }
+
+  handleParentData(rawData, stepName, pageName, onCheck) {
+    rawData && rawData.forEach(el => {
+      if (stepName) {
+        el.showOnRequest = true
+      }
+      switch (el.fieldName) {
+        case 'emailaddress':
+          el.isCheck = true
+          el.onCheck = onCheck
+          break
+        case 'stafftype':
+          el.itemList = el.itemList.filter(_ => { return _.type !== 'Head Office(PYN)' })
+          break
+        case 'ownerstafftype':
+          el.itemList = el.itemList.filter(_ => { return _.type !== 'Head Office(PYN)' })
+          break
+        default:
+          return rawData
+      }
+    })
+    switch (stepName) {
+      case 'T3':
+        break
+      default:
+        return rawData
+    }
+  }
+
+  beforeSubmit(dataMap) {
+    return dataMap
+  }
+
+  getChildTableTitle() {
+    return 'Child Table'
+  }
+
+  getChildFormTitle() {
+    return 'Child'
+  }
+
+  setSupervisorEmail(value, dataMap, checkName) {
+    dataMap.set(checkName, { id: checkName, label: value, value })
+  }
+
+  getReturnType(parentDataMap, fieldName) {
+    let returnType = null
+    switch (fieldName) {
+      case 'emailaddress':
+        returnType = 'user'
+        break
+      default:
+        break
+    }
+    if ((!parentDataMap.get(fieldName) || !parentDataMap.get(fieldName).value)) {
+      CommonTip.error('E-mail Address is required')
+      returnType = null
+    }
+    return returnType
+  }
+
+  getContractList() {
+    return false
+  }
+}
+
