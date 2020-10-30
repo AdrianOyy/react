6bc7cebad8e3e5383d5f23b980c99e913eacc05c
APJ\lituoxin <lukeli@apjcorp.com>
2020-10-30 10:09:39 +0800

    status处理

--- Code/src/components/CommonWorkflowForm/CommonWorkflowForm.js
+++ Code/src/components/CommonWorkflowForm/CommonWorkflowForm.js
@@ -514,7 +514,16 @@ export default function CommonWorkflowForm(props) {
 
   const handleT1FollowUpClick = () => {
     const childData = map2object(childDataMap)
-    childData.status.value = 'skip'
+    console.log(childData)
+    if (childData.status) {
+      childData.status.value = 'skip'
+    } else {
+      childData.status = {
+        id: 'status',
+        label: 'skip',
+        value: 'skip'
+      }
+    }
     childData.checkState = true
     childData.id = childDataList[current].id
     childDataList[current] = childData
@@ -523,7 +532,15 @@ export default function CommonWorkflowForm(props) {
   }
   const handleT2FollowUpClick = () => {
     const childData = map2object(childDataMap)
-    childData.status.value = 'skip'
+    if (childData.status) {
+      childData.status.value = 'skip'
+    } else {
+      childData.status = {
+        id: 'status',
+        label: 'skip',
+        value: 'skip'
+      }
+    }
     childData.checkState = true
     childData.id = childDataList[current].id
     childDataList[current] = childData
@@ -532,7 +549,15 @@ export default function CommonWorkflowForm(props) {
   }
   const handleT6FollowUpClick = () => {
     const childData = map2object(childDataMap)
-    childData.status.value = 'skip'
+    if (childData.status) {
+      childData.status.value = 'skip'
+    } else {
+      childData.status = {
+        id: 'status',
+        label: 'skip',
+        value: 'skip'
+      }
+    }
     childData.checkState = true
     childData.id = childDataList[current].id
     childDataList[current] = childData
