be59835ce71307ab09407dad5ce8783d6c780ded
amaneru <amaneru@163.com>
2020-12-09 17:27:30 +0800

    脚本更新

--- Code/env.sh
+++ Code/env.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-
-find './usr/share/nginx/sense' -name '*.js' -exec sed -i -e 's/API_BASE_URL/'"${API_BASE_URL}"'/g' {} \;
-
-nginx -g "daemon off;";
+find './usr/share/nginx/sense' -name '*.js' -exec sed -i -e 's!API_BASE_URL!'"${API_BASE_URL}"'!g' {} \; && nginx -g "daemon off;";
